7fe19c3b868cf574d44b28d3112f433c7a4718b6
Subhasish Sinha <77578582+subhasishsinha12@users.noreply.github.com>
2026-02-22 07:43:57 +0530

    feat(layer-01): add Winston logger utility

---
+++ layer-01-donor/src/utils/logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    winston.format.printf(({ timestamp, level, message }) => 
+      `[${timestamp}] ${level}: ${message}`
+    )
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' })
+  ]
+});
+
+module.exports = logger;
